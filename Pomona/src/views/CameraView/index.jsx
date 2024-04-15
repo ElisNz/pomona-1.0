d10aa2d52cf73cfce6eadaeb7601d2b09fabbeed
E.N <67209588+ElisNz@users.noreply.github.com>
2024-04-15 19:27:44 +0900

    added functionality for taking pictures and

--- Pomona/src/views/CameraView/index.jsx
+++ Pomona/src/views/CameraView/index.jsx
@@ -1,17 +1,21 @@
 import { useRef, useState } from 'react';
 import { Camera, CameraType } from 'expo-camera';
 import { View, Pressable, Platform, Text } from 'react-native';
-import Svg, { Path } from "react-native-svg";
+import Svg, { path } from "react-native-svg";
 import styles from './styles';
 
 const CameraView = () => {
 	const videoRef = useRef(null);
-	const photoRef = useRef(null);
-	const [hasPhoto, setHasPhoto] = useState(false);
-
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
   const type = Platform.OS !== 'web' ? CameraType.back : CameraType.front;
+
+	const __takePicture = async () => {
+		if (!videoRef.current) return;
+		
+		const photo = await videoRef.current.takePictureAsync({ base64: true });
+		console.log(photo);
+	};
 
   return (
 		<View style={styles.container}>
@@ -27,19 +31,25 @@ const CameraView = () => {
 				</View>
 			}
 			{permission?.granted &&
-				<View style={styles.cameraContainer}>
-					<Camera 
-						style={styles.camera}
-						type={type}
-						ref={videoRef}
-					/>
-					<View style={styles.overlay}>
-						<Svg viewBox="0 0 21 21" fill="#000000">
-							<g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier">
-								<path d="m16.5 5.5v-3.00587878c0-1.10227102-.8918463-1.99674657-1.9941126-1.99999134l-3.0058874-.00884851m5 11.01471863v3c0 1.1045695-.8954305 2-2 2h-3m-6-16.01471863-3.00588742.00884851c-1.10226624.00324477-1.99411258.89772032-1.99411258 1.99999134v3.00587878m5 11h-3c-1.1045695 0-2-.8954305-2-2v-3" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 2)"></path>
-							</g>
-						</Svg>
+				<View style={styles.content}>
+					<View style={styles.cameraContainer}>
+						<Camera 
+							style={styles.camera}
+							type={type}
+							focusDepth={1}
+							ref={videoRef}
+						/>
+						<View style={styles.overlay}>
+							<Svg viewBox="0 0 21 21" fill="#000000">
+								<g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier">
+									<path d="m16.5 5.5v-3.00587878c0-1.10227102-.8918463-1.99674657-1.9941126-1.99999134l-3.0058874-.00884851m5 11.01471863v3c0 1.1045695-.8954305 2-2 2h-3m-6-16.01471863-3.00588742.00884851c-1.10226624.00324477-1.99411258.89772032-1.99411258 1.99999134v3.00587878m5 11h-3c-1.1045695 0-2-.8954305-2-2v-3" fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 2)"></path>
+								</g>
+							</Svg>
+						</View>
 					</View>
+					<Pressable style={styles.button} onPress={__takePicture} title="take picture">
+						<Text style={{color: 'white', textAlign: 'center', fontSize: '20px'}}>Take Picture</Text>
+					</Pressable>
 				</View>
 			}
 		</View>
--- Pomona/src/views/CameraView/styles.ts
+++ Pomona/src/views/CameraView/styles.ts
@@ -2,15 +2,28 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    marginTop: 50,
+    marginBottom: 50,
     width:'50%',
     height: '50%',
     flexDirection: 'column',
+    gap: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    width: '100%',
+    height: '100%',
+    paddingTop: 50,
+    flex: 1,
+    gap: 30,
+    flexDirection: 'column',
     alignItems: 'center',
   },
   cameraContainer: {
     width: '50%',
-    height: '100%',
+    height: '50%',
     overflow: 'hidden',
     borderRadius: 60,
   },
@@ -24,6 +37,17 @@ const styles = StyleSheet.create({
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    width: '50%',
+    height: '10%',
+    padding: 10,
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: 'black',
+    borderRadius: 10,
+    textAlign: 'center',
+    justifyContent: 'center',
   },
 });
 
