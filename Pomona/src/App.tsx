600d46863454e9a95d819ed15356f0e43a024607
E.N <67209588+ElisNz@users.noreply.github.com>
2024-04-15 14:30:23 +0900

    added location tracking and context

--- Pomona/src/App.tsx
+++ Pomona/src/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import Main from './pages/main';
+import { LocationProvider } from './context/locationContext';
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LocationProvider>
+      <Main />
+    </LocationProvider>
   );
 }
