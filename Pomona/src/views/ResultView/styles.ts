b629dfe81355a2094204cad245f82a4a0d307eae
E.N <67209588+ElisNz@users.noreply.github.com>
2024-04-20 23:45:49 +0900

    added styling for the result view

---
+++ Pomona/src/views/ResultView/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: '50%',
+    height: '10%',
+    padding: 10,
+    marginTop: 30,
+    marginBottom: 30,
+    backgroundColor: 'black',
+    borderRadius: 10,
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default styles;
