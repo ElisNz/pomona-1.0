0249bd3cf8d523c4bbc433667c6af5cde60ea5bc
E.N <67209588+ElisNz@users.noreply.github.com>
2024-05-24 13:12:33 +0200

    fixed camera component for use on android

--- Pomona/src/views/CameraView/index.jsx
+++ Pomona/src/views/CameraView/index.jsx
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 import { Camera, CameraType } from 'expo-camera';
 import { View, Pressable, Platform, Text } from 'react-native';
-import Svg, { path } from "react-native-svg";
+import Svg, { Path } from "react-native-svg";
 import styles from './styles';
 
 const CameraView = ({ setImg }) => {
@@ -43,22 +43,19 @@ const CameraView = ({ setImg }) => {
 						/>
 						<View style={styles.overlay}>
 							<Svg viewBox="0 0 21 21" fill="#000000">
-								<g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-								<g id="SVGRepo_iconCarrier">
-									<path 
+									<Path 
 										d="m16.5 5.5v-3.00587878c0-1.10227102-.8918463-1.99674657-1.9941126-1.99999134l-3.0058874-.00884851m5 11.01471863v3c0 1.1045695-.8954305 2-2 2h-3m-6-16.01471863-3.00588742.00884851c-1.10226624.00324477-1.99411258.89772032-1.99411258 1.99999134v3.00587878m5 11h-3c-1.1045695 0-2-.8954305-2-2v-3" 
 										fill="none" 
 										stroke="#FFFFFF33" 
 										strokeLinecap="round" 
 										strokeLinejoin="round" 
 										transform="translate(2 2)">
-									</path>
-								</g>
+									</Path>
 							</Svg>
 						</View>
 					</View>
 					<Pressable style={styles.button} onPress={__takePicture} title="take picture">
-						<Text style={{color: 'white', textAlign: 'center', fontSize: '20px'}}>Take Picture</Text>
+						<Text style={{color: 'white', textAlign: 'center', fontSize: 20}}>Take Picture</Text>
 					</Pressable>
 				</View>
 			}
